c990a1ceb09a4cefc015ec5e89940ce2d40a8dd7
Chidi Ndego <chidindego@gmail.com>
2021-12-24 20:30:29 +0100

    requests exercise

--- course-ud303/Lesson-2/6_UsingJSON/UINames.py
+++ course-ud303/Lesson-2/6_UsingJSON/UINames.py
@@ -15,6 +15,10 @@ def SampleRecord():
     r = requests.get("http://uinames.com/api?ext&region=United%20States",
                      timeout=2.0)
     # 1. Add a line of code here to decode JSON from the response.
+    text = r.json()
+    print(text)
+
+    # Link no longer working
 
     return "My name is {} {} and the PIN on my card is {}.".format(
         # 2. Add the correct fields from the JSON data structure.
